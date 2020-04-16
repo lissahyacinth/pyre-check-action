3a4984be616e33534315cc28a98ee9c678d06679
Lissa Hyacinth <lissa.hyacinth@forward3d.com>
2020-04-16 15:39:50 +0100

    Add pip_arg variable to install dependencies in workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@ set -uo pipefail
 
 GITHUB_TOKEN=$1
 PYRE_ARGS=$2
+PIP_ARGS=$3
 
 post_pr_comment() {
   local msg=$1
@@ -15,7 +16,18 @@ post_pr_comment() {
     "${request_url}" > /dev/null
 }
 
+install_dependencies() {
+ local pip_args=$1
+ if [ -z $1 ]
+ then
+    return 0
+ else
+    pip install $1
+ fi
+}
+
 main() {
+  install_dependencies "${PIP_ARGS}"
   pyre --source-directory . check > /tmp/PyreOutput.txt
   comment_title="Pyre Output"
   comment_body=""
